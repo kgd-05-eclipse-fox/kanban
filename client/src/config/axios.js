8730bb468bb69f8ec22cb1cf6755a4371ff21482
akbarhabiby <akbarhabiby@icloud.com>
2020-11-06 14:46:23 +0700

    Changed baseURL for axios

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:3000'
+    baseURL: 'https://kanban-akbarhabiby.herokuapp.com/'
 })
 
 export default instance
