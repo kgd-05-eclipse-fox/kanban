a772877819d7870d0bab6b60f6fa812e062888c6
bimakrishna <krishna.bima@yahoo.com>
2020-11-06 23:53:01 +0700

    bimakrishna

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV != 'production') {
 const express = require("express")
 const cors = require("cors")
 const app = express()
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT
 const router = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
 
