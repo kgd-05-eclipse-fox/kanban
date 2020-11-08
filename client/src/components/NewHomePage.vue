d938e244eb5d403aeb15b9f8802f75ab6997fa88
abrarnaim015 <aabrarnaim24@gmail.com>
2020-11-08 10:56:45 +0700

    Update add Compenents card and data kanban

---
+++ client/src/components/NewHomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: 'NewHomePage',
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    },
+    components:{
+
+    },
+    created(){
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
