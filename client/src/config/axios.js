aa4f659d152465051849b85e9785241e98065d2d
torianyap <torianyap@gmail.com>
2020-11-20 10:38:35 +0700

    add socket io

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://kanban-tori.herokuapp.com',
+  baseURL: 'https://kanban-tori.herokuapp.com',
 });
 
 export default instance
--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import GoogleSignInButton from 'vue-google-signin-button-directive'
+import VueSocketIO from 'vue-socket.io'
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'https://kanban-tori.herokuapp.com'
+}))
 
 new Vue({
     render: h => h(App),
