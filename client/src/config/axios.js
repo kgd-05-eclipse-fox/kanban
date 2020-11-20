432b854f076d124bfab2b7cc5f8c277efd10745f
abrarnaim015 <aabrarnaim24@gmail.com>
2020-11-20 10:47:46 +0700

    fixed all bismillah

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    // baseURL: 'https://wolf-kanban-naim.herokuapp.com'
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://wolf-kanban-naim.herokuapp.com'
+    // baseURL: 'http://localhost:3000'
 })
 
 export default instance
